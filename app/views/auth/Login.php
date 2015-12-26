3ca071dbb2ff8ee667656dda74fb2dce57e17733
DaVaR <davar@thedavar.net>
2015-12-25 22:27:24 -0600

    Updates
    Updated some styles

--- app/views/auth/Login.php
+++ app/views/auth/Login.php
@@ -2,28 +2,36 @@
 use \Helpers\Form,
 	\Core\Error; 	
 ?>
-
-<div class='panel panel-info' style='max-width: 500px' align='center'>
-	<div class='panel-heading'>
-		<div class='panel-title'>Login</div>
-	</div>
-	<div class='pannel-body' style='padding:10px' align='center'>
-
-		<?php echo Error::display($error); ?>
-	
-		<?php echo Form::open(array('method' => 'post')); ?>
-
-			<div class='input-group' style='width: 80%; margin-bottom: 25px'>
-				<span class='input-group-addon'><i class='glyphicon glyphicon-user'></i></span>
-				<?php echo Form::input(array('name' => 'username', 'class' => 'form-control', 'placeholder' => 'UserName or Email')); ?>
+<div class='col-lg-8'>
+	<div class='panel panel-default'>
+		<div class='panel-heading'>
+			<h3 class='jumbotron-heading'><?php echo $data['title'] ?></h3>
+		</div>
+		<div class='panel-body'>
+			<p><?php echo $data['welcome_message'] ?></p>
+			<div align=center>
+				<!-- Display Login Box -->
+				<div class='panel panel-info' style='max-width: 500px' align='center'>
+					<div class='panel-heading'>
+						<div class='panel-title'>Login</div>
+					</div>
+					<div class='pannel-body' style='padding:10px' align='center'>
+						<?php echo Error::display($error); ?>
+						<?php echo Form::open(array('method' => 'post')); ?>
+							<div class='input-group' style='width: 80%; margin-bottom: 25px'>
+								<span class='input-group-addon'><i class='glyphicon glyphicon-user'></i></span>
+								<?php echo Form::input(array('name' => 'username', 'class' => 'form-control', 'placeholder' => 'UserName or Email')); ?>
+							</div>
+							<div class='input-group' style='width: 80%; margin-bottom: 25px'>
+								<span class='input-group-addon'><i class='glyphicon glyphicon-lock'></i></span>
+								<?php echo Form::input(array('type' => 'password', 'name' => 'password', 'class' => 'form-control', 'placeholder' => 'Password')); ?>
+							</div>
+							<?php echo Form::input(array('type' => 'submit', 'name' => 'submit', 'value' => 'Submit', 'class' => 'btn btn-success btn-sm')); ?>
+						<?php echo Form::close(); ?>
+					</div>
+				</div>
+				<!-- End Display Login Box -->
 			</div>
-			<div class='input-group' style='width: 80%; margin-bottom: 25px'>
-				<span class='input-group-addon'><i class='glyphicon glyphicon-lock'></i></span>
-				<?php echo Form::input(array('type' => 'password', 'name' => 'password', 'class' => 'form-control', 'placeholder' => 'Password')); ?>
-			</div>
-			<?php echo Form::input(array('type' => 'submit', 'name' => 'submit', 'value' => 'Submit', 'class' => 'btn btn-success btn-sm')); ?>
-
-		<?php echo Form::close(); ?>
-		
+		</div>
 	</div>
 </div>
--- app/views/welcome/subpage.php
+++ app/views/welcome/subpage.php
@@ -6,13 +6,17 @@
 use Core\Language;
 
 ?>
+<div class='col-lg-8'>
+	<div class='panel panel-default'>
+		<div class='panel-heading'>
+			<h3 class='jumbotron-heading'><?php echo $data['title'] ?></h3>
+		</div>
+		<div class='panel-body'>
+			<p><?php echo $data['welcome_message'] ?></p>
 
-<div class="page-header">
-	<h1><?php echo $data['title'] ?></h1>
+			<a class="btn btn-md btn-success" href="<?php echo DIR;?>">
+				<?php echo Language::show('back_home', 'Welcome'); ?>
+			</a>
+		</div>
+	</div>
 </div>
-
-<p><?php echo $data['welcome_message'] ?></p>
-
-<a class="btn btn-md btn-success" href="<?php echo DIR;?>">
-	<?php echo Language::show('back_home', 'Welcome'); ?>
-</a>
